721cc5ce94babda4a0b42ee0a734d072ff10a291
ih-abir <ih.abir.1@gmail.com>
2022-06-29 10:04:59 +0600

    blog pages

--- src/queries/AllPagesQuery.js
+++ src/queries/AllPagesQuery.js
@@ -287,6 +287,102 @@ const AllPagesQuery = `
       }
     }
   }
+  homeBlog {
+    data {
+      id
+      attributes {
+        localizations {
+        data {
+          attributes {
+            locale
+              Meta{
+                HTML_Title
+                Meta_description
+                noindex
+                nofollow
+                URL_slug
+                Canonical_link
+              }
+            }
+          }
+        }
+        locale
+        Meta{
+        HTML_Title
+        Meta_description
+        noindex
+        nofollow
+        URL_slug
+        Canonical_link
+        }
+      }
+    }
+  }
+  blogs {
+    data {
+      id
+      attributes {
+        localizations {
+        data {
+          attributes {
+            locale
+              Title
+              Intro_text
+              Intro_blob{
+                data{
+                  id
+                  attributes{
+                    url
+                    provider_metadata
+                    formats
+                    alternativeText
+                  }
+                }
+              }
+              Block_text
+              createdAt
+              updatedAt
+              publishedAt
+              Meta{
+                HTML_Title
+                Meta_description
+                noindex
+                nofollow
+                URL_slug
+                Canonical_link
+              }
+            }
+          }
+        }
+        locale
+        Title
+        Intro_text
+        Intro_blob{
+          data{
+            id
+            attributes{
+              url
+              provider_metadata
+              formats
+              alternativeText
+            }
+          }
+        }
+        Block_text
+        createdAt
+        updatedAt
+        publishedAt
+        Meta{
+          HTML_Title
+          Meta_description
+          noindex
+          nofollow
+          URL_slug
+          Canonical_link
+        }
+      }
+    }
+  }
 }`;
 
 export default AllPagesQuery;
