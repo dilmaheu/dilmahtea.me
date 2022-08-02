16a7dc72135915a77e8f7bca815123729b724be4
Rafid Muhymin Wafi <rafidmuhymin@gmail.com>
2022-08-02 09:41:27 +0000

    fixed error Promise.all.reverse isn't a function

--- generateHeaders.mjs
+++ generateHeaders.mjs
@@ -16,60 +16,63 @@ const commonPolicies = {
 };
 
 const CSPHeaders = await Promise.all(
-  htmlFilePaths.map(async (path) => {
-    const htmlContent = await fs.readFile(path, "utf8");
+  htmlFilePaths
+    .map(async (path) => {
+      const htmlContent = await fs.readFile(path, "utf8");
 
-    const { document } = await parseHTML(htmlContent);
+      const { document } = await parseHTML(htmlContent);
 
-    const scripts = document.querySelectorAll("script");
+      const scripts = document.querySelectorAll("script");
 
-    const route =
-      path === "./dist/404.html"
-        ? "/*"
-        : path === "./dist/index.html"
-        ? "/"
-        : path.slice(6, -11);
+      const route =
+        path === "./dist/404.html"
+          ? "/*"
+          : path === "./dist/index.html"
+          ? "/"
+          : path.slice(6, -11);
 
-    const nonces = scripts.map((script) => {
-      const nonce = crypto.randomUUID();
+      const nonces = scripts.map((script) => {
+        const nonce = crypto.randomUUID();
 
-      script.setAttribute("nonce", nonce);
+        script.setAttribute("nonce", nonce);
 
-      return `'nonce-${nonce}'`;
-    });
+        return `'nonce-${nonce}'`;
+      });
 
-    const policies = {
-      ...commonPolicies,
-      "script-src": ["'self'", "https://static.openreplay.com", ...nonces],
-    };
+      const policies = {
+        ...commonPolicies,
+        "script-src": ["'self'", "https://static.openreplay.com", ...nonces],
+      };
 
-    const CSPHeader = Object.keys(policies)
-      .map((directive) => {
-        const values = policies[directive];
+      const CSPHeader = Object.keys(policies)
+        .map((directive) => {
+          const values = policies[directive];
 
-        const policy =
-          values.length === 0 ? directive : `${directive} ${values.join(" ")}`;
+          const policy =
+            values.length === 0
+              ? directive
+              : `${directive} ${values.join(" ")}`;
 
-        return policy;
-      })
-      .join("; ");
+          return policy;
+        })
+        .join("; ");
 
-    document.head.insertAdjacentHTML(
-      "beforeend",
-      `<meta http-equiv="Content-Security-Policy" content="${CSPHeader}">`
-    );
+      document.head.insertAdjacentHTML(
+        "beforeend",
+        `<meta http-equiv="Content-Security-Policy" content="${CSPHeader}">`
+      );
 
-    // overwrite html to add generated csp header & nonces
-    await fs.writeFile(path, document.documentElement.outerHTML);
+      // overwrite html to add generated csp header & nonces
+      await fs.writeFile(path, document.documentElement.outerHTML);
 
-    return {
-      route,
-      CSPHeader,
-    };
-  })
-)
-  // put 404 page csp header at end
-  .reverse();
+      return {
+        route,
+        CSPHeader,
+      };
+    })
+    // put 404 page csp header at end
+    .reverse()
+);
 
 const _headersFileContent = CSPHeaders.map(
   ({ route, CSPHeader }) => `${route}\n  Content-Security-Policy: ${CSPHeader}`
