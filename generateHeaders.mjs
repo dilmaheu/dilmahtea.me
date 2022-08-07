87029bdd0e8eea8b6c2317f75cfff60b1e1d23cb
Rafid Muhymin Wafi <rafidmuhymin@gmail.com>
2022-08-07 11:29:53 +0000

    re: try to load env vars from cloudflare

--- generateHeaders.mjs
+++ generateHeaders.mjs
@@ -86,6 +86,10 @@ const _headersFileContent =
 // write generated headers to _headers file
 await fs.writeFile("./dist/_headers", _headersFileContent);
 
+console.log(process.env)
+
+console.log(globalThis)
+
 const endpoint = `https://api.cloudflare.com/client/v4/zones/${context.env.ZONE_ID}/rulesets/${context.env.RULESET_ID}`;
 
 const response = await fetch(endpoint, {
