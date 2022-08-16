283d718b02d0e0c2d53379da9a537ab1861f520b
Rafid Muhymin Wafi <rafidmuhymin@gmail.com>
2022-08-16 11:39:06 +0000

    localize 404 page on dev server

--- src/queries/NotFoundPagesQuery.js
+++ src/queries/NotFoundPagesQuery.js
@@ -1,9 +1,33 @@
 const NotFoundPagesQuery = `
   {
-    basicPages(locale: "all", filters: { Meta: { URL_slug: { eq: "404" } } }) {
+    basicPages(locale: "en", filters: { Meta: { URL_slug: { eq: "404" } } }) {
       data {
-        id
+        id        
         attributes {
+          locale
+          Title
+          Intro_text
+          Heading_block
+          Block_text
+          Block_blob {
+            data {
+              id
+              attributes {
+                url
+                provider_metadata
+                formats
+                alternativeText
+              }
+            }
+          }
+          Meta {
+            HTML_Title
+            Meta_description
+            noindex
+            nofollow
+            URL_slug
+            Canonical_link
+          }
           localizations {
             data {
               attributes {
@@ -34,30 +58,6 @@ const NotFoundPagesQuery = `
               }
             }
           }
-          locale
-          Title
-          Intro_text
-          Heading_block
-          Block_text
-          Block_blob {
-            data {
-              id
-              attributes {
-                url
-                provider_metadata
-                formats
-                alternativeText
-              }
-            }
-          }
-          Meta {
-            HTML_Title
-            Meta_description
-            noindex
-            nofollow
-            URL_slug
-            Canonical_link
-          }
         }
       }
     }
@@ -77,6 +77,17 @@ const NotFoundPagesQuery = `
                 url
                 provider_metadata
                 alternativeText
+              }
+            }
+          }
+          localizations {
+            data {
+              attributes {
+                locale
+                Footer_text
+                Company_name
+                Company_address
+                Company_email
               }
             }
           }
