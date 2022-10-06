0dd5ab82a1041183e6d072601ca53128122d8468
Rafid Muhymin Wafi <rafidmuhymin@gmail.com>
2022-10-06 12:06:06 +0000

    handle graphql errors

--- src/store/CMS.js
+++ src/store/CMS.js
@@ -30,33 +30,26 @@ const { data } = await fetch(import.meta.env.DB_URL, {
   }),
 })
   .then(async (res) => {
-    const data = await res.text(),
-      cacheDir = "./.cache/";
+    const cacheDir = "./.cache/",
+      response = await res.text(),
+      data = JSON.parse(response);
+
+    if (data.errors) {
+      return catchError(data.errors);
+    }
 
     fs.existsSync(cacheDir) || (await fs.promises.mkdir(cacheDir));
-    await fs.promises.writeFile(cacheDir + "CMS.json", data, "utf8");
+
+    await fs.promises.writeFile(cacheDir + "CMS.json", response, "utf8");
 
     printMessage(
       "info",
       "Successfully fetched data from CMS and saved it to the cache"
     );
 
-    return JSON.parse(data);
+    return data;
   })
-  .catch(async (error) => {
-    if (fs.existsSync("./.cache/CMS.json")) {
-      const data = await fs.promises.readFile("./.cache/CMS.json", "utf8");
-
-      printMessage(
-        "error",
-        "Failed to fetch data from CMS, serving from the cache"
-      );
-
-      return JSON.parse(data);
-    }
-
-    throw error;
-  });
+  .catch(catchError);
 
 const CMS = {
   get(contentType) {
@@ -65,5 +58,31 @@ const CMS = {
     return data[contentType];
   },
 };
+
+async function catchError(error) {
+  if (fs.existsSync("./.cache/CMS.json")) {
+    const cachedData = JSON.parse(
+      await fs.promises.readFile("./.cache/CMS.json", "utf8")
+    );
+
+    printMessage(
+      "error",
+      "Failed to fetch data from CMS, serving from the cache"
+    );
+
+    return cachedData;
+  } else {
+    printMessage(
+      "error",
+      "Failed to fetch data from CMS, nothing found in cache!"
+    );
+  }
+
+  if (error.message) {
+    throw error;
+  } else {
+    console.error(error);
+  }
+}
 
 export default CMS;
