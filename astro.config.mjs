7c45aa6c641cd1ec9813e525e34bd76fbcb177f3
RafidMuhymin <rafidmuhymin@gmail.com>
2022-11-23 02:37:31 +0600

    removed @astrojs/sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-import sitemap from "@astrojs/sitemap";
 import taiwlind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import { astroImageTools } from "astro-imagetools";
@@ -7,23 +6,7 @@ import PermissionsPolicy from "./src/store/PermissionsPolicy.js";
 export default defineConfig({
   site: "https://dilmahtea.me",
 
-  integrations: [
-    // sitemap({
-    //   serialize(item) {
-    //     if (/exclude-from-sitemap/.test(item.url)) {
-    //       return undefined;
-    //     }
-    //     if (/your-special-page/.test(item.url)) {
-    //       item.changefreq = "daily";
-    //       item.lastmod = new Date();
-    //       item.priority = 0.9;
-    //     }
-    //     return item;
-    //   },
-    // }),
-    taiwlind(),
-    astroImageTools,
-  ],
+  integrations: [taiwlind(), astroImageTools],
 
   vite: {
     ssr: {
