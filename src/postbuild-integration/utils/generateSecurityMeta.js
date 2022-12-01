319bdd3f598986df250214e5af5974bba87b0c56
ih-abir <ih.abir.1@gmail.com>
2022-12-01 11:26:46 +0600

    added securty.txt from cms

---
+++ src/postbuild-integration/utils/generateSecurityMeta.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import fs from "fs/promises";
+import fetch from "node-fetch";
+
+dotenv.config({ path: "./.env" });
+
+const CMS_ENDPOINT = process.env.Assets_URL,
+  ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+
+const response = await fetch(`${CMS_ENDPOINT}/api/security-text`, {
+  method: "GET",
+  headers: { Authorization: `Bearer ${ACCESS_TOKEN}` },
+});
+
+const securityMetaData = await response.json();
+
+const securityTXT = securityMetaData.data.attributes.Text;
+
+await fs.writeFile("./dist/security.txt", securityTXT);
