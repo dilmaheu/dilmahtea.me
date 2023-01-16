a373b60ce2e5e042668eeada7193e6d1400d9c23
RafidMuhymin <rafidmuhymin@gmail.com>
2023-01-17 00:41:43 +0600

    localized recurring images

--- src/store/RecurringImages.js
+++ src/store/RecurringImages.js
@@ -1,16 +1,35 @@
+import fs from "node:fs";
 import CMS from "@store/CMS";
 
 const { data: recurringImageData } = CMS.get("recurringImage");
 
 const RecurringImages = {};
 
-Object.keys(recurringImageData.attributes).map((key) => {
-  const { url: relativeSrc, alternativeText: alt } =
-    recurringImageData.attributes[key].data.attributes;
+await Promise.all(
+  Object.keys(recurringImageData.attributes).map(async (key) => {
+    const { url: relativeSrc, alternativeText: alt } =
+      recurringImageData.attributes[key].data.attributes;
 
-  const src = import.meta.env.ASSETS_URL + relativeSrc;
+    const src = "/images/" + relativeSrc.slice(9),
+      imagesDir = "./public/images/",
+      imagePath = "./public" + src;
 
-  RecurringImages[key] = { src, alt };
-});
+    if (!fs.existsSync(imagesDir)) {
+      await fs.promises.mkdir(imagesDir, { recursive: true });
+    }
+
+    if (!fs.existsSync(imagePath)) {
+      const remoteSrc = import.meta.env.ASSETS_URL + relativeSrc;
+
+      const imageBuffer = Buffer.from(
+        await fetch(remoteSrc).then((res) => res.arrayBuffer())
+      );
+
+      await fs.promises.writeFile(imagePath, imageBuffer);
+    }
+
+    RecurringImages[key] = { src, alt };
+  })
+);
 
 export default RecurringImages;
