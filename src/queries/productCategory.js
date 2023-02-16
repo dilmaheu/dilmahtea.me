c481c55fa3e12a94819f1b91030e0f822cf25790
ih-abir <ih.abir.1@gmail.com>
2023-02-16 18:09:05 +0600

    Categorized product page added

---
+++ src/queries/productCategory.js
@@ -0,0 +1,129 @@
+const productCategoriesQuery = `
+  {
+    productCategories {
+      data {
+        id
+        attributes {
+          locale
+          updatedAt
+          Title
+          Intro_text
+          Intro_blob {
+            data {
+              id
+              attributes {
+                url
+                provider_metadata
+                formats
+                alternativeText
+              }
+            }
+          }
+          Heading_button_text
+          Heading_button_link
+          Block_text
+          Sub_categories {
+            data {
+              attributes {
+                Title
+                Intro_text
+              }
+            }
+          }
+          Relevant_items_title
+          Relevant_recipes {
+            data{
+              attributes{
+                Title
+                Intro_text
+                Intro_blob{
+                  data{
+                    attributes{
+                      url
+                      provider_metadata
+                      alternativeText
+                    }
+                  }
+                }
+                Meta{
+                  URL_slug
+                }
+              }
+            }
+          }
+          Meta {
+            HTML_Title
+            Meta_description
+            noindex
+            nofollow
+            URL_slug
+            Canonical_link
+          }
+          localizations {
+            data {
+              attributes {
+                locale
+                updatedAt
+                Title
+                Intro_text
+                Intro_blob {
+                  data {
+                    id
+                    attributes {
+                      url
+                      provider_metadata
+                      formats
+                      alternativeText
+                    }
+                  }
+                }
+                Heading_button_text
+                Heading_button_link
+                Block_text
+                Sub_categories {
+                  data {
+                    attributes {
+                      Title
+                      Intro_text
+                    }
+                  }
+                }
+                Relevant_items_title
+                Relevant_recipes {
+                  data{
+                    attributes{
+                      Title
+                      Intro_text
+                      Intro_blob{
+                        data{
+                          attributes{
+                            url
+                            provider_metadata
+                            alternativeText
+                          }
+                        }
+                      }
+                      Meta{
+                        URL_slug
+                      }
+                    }
+                  }
+                }
+                Meta {
+                  HTML_Title
+                  Meta_description
+                  noindex
+                  nofollow
+                  URL_slug
+                  Canonical_link
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default productCategoriesQuery;
