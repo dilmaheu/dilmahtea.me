593ddff5ed3803f29d6a7ec751ebe9a875106362
ih-abir <ih.abir.1@gmail.com>
2023-03-15 13:41:22 +0600

    dashboard query update

--- src/queries/userAccountDashboard.js
+++ src/queries/userAccountDashboard.js
@@ -7,24 +7,17 @@ const userAccountDashboardQuery = `
           Title
           Personal_information{
             Name
-            Phone
-            Email
-            Address
+            Label_username
             Label_phone
             Label_email
-            Label_address
+            Label_delivery_address
+            Label_billing_address
+            Social_media_title
             Social_media{
               Facebook
               Google
               Twitter
             }
-            Social_media_title
-            Button_remove_text
-            Button_update_text
-            Button_edit_text
-            Button_add_new_address_text
-            text_connected
-            text_not_connected
           }
           Block2_visibility
           Block2{
@@ -34,35 +27,9 @@ const userAccountDashboardQuery = `
             My_selected_cause_text
             Label_let_dilmah_pick_cause
             Let_dilmah_pick_cause_text
-            Button_change_cause_text
             Notification_cause_update_text
           }
-          Block3_visibility
-          Block3{
-            Block3_title
-            Label_order
-            Button_track_package
-            Button_track_package_text
-            Button_buy_again_text
-            Button_sold_out_text
-            Button_view_all_orders_text
-            text_more_products_in_this_order
-            text_estimated_delivery
-            text_estimated_shipment
-            text_delivered
-            Empty_order_blob{
-              data{
-                attributes{
-                  url
-                  provider_metadata
-                  alternativeText
-                }
-              }
-            }
-            Empty_order_text
-            Empty_order_button_text
-            Empty_order_button_link
-          }
+          Block3_title
           Support_visibility
           Support{
             Button_text
@@ -83,7 +50,6 @@ const userAccountDashboardQuery = `
               Link
             }
           }
-          text_content
           Meta {
             HTML_Title
             Meta_description
@@ -99,24 +65,17 @@ const userAccountDashboardQuery = `
                 Title
                 Personal_information{
                   Name
-                  Phone
-                  Email
-                  Address
+                  Label_username
                   Label_phone
                   Label_email
-                  Label_address
+                  Label_delivery_address
+                  Label_billing_address
+                  Social_media_title
                   Social_media{
                     Facebook
                     Google
                     Twitter
                   }
-                  Social_media_title
-                  Button_remove_text
-                  Button_update_text
-                  Button_edit_text
-                  Button_add_new_address_text
-                  text_connected
-                  text_not_connected
                 }
                 Block2_visibility
                 Block2{
@@ -124,38 +83,11 @@ const userAccountDashboardQuery = `
                   Block2_text
                   Label_my_selected_cause
                   My_selected_cause_text
-                  My_selected_cause_text
                   Label_let_dilmah_pick_cause
                   Let_dilmah_pick_cause_text
-                  Button_change_cause_text
                   Notification_cause_update_text
                 }
-                Block3_visibility
-                Block3{
-                  Block3_title
-                  Label_order
-                  Button_track_package
-                  Button_track_package_text
-                  Button_buy_again_text
-                  Button_sold_out_text
-                  Button_view_all_orders_text
-                  text_more_products_in_this_order
-                  text_estimated_delivery
-                  text_estimated_shipment
-                  text_delivered
-                  Empty_order_blob{
-                    data{
-                      attributes{
-                        url
-                        provider_metadata
-                        alternativeText
-                      }
-                    }
-                  }
-                  Empty_order_text
-                  Empty_order_button_text
-                  Empty_order_button_link
-                }
+                Block3_title
                 Support_visibility
                 Support{
                   Button_text
@@ -176,7 +108,6 @@ const userAccountDashboardQuery = `
                     Link
                   }
                 }
-                text_content
                 Meta {
                   HTML_Title
                   Meta_description
