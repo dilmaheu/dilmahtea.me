81e91857ad3966ac30f2654d8c975523f24d3242
RafidMuhymin <rafidmuhymin@gmail.com>
2023-04-06 14:47:32 +0600

    added renderMarkdown utility

---
+++ src/utils/renderMarkdown.ts
@@ -0,0 +1,16 @@
+import {
+  MarkdownRenderingOptions,
+  renderMarkdown as astroMarkdownRemarkRenderMarkdown,
+} from "@astrojs/markdown-remark";
+
+export default async function renderMarkdown(
+  markdown: string,
+  opts?: MarkdownRenderingOptions
+): Promise<string> {
+  const { code } = await astroMarkdownRemarkRenderMarkdown(
+    markdown,
+    opts ?? {}
+  );
+
+  return code;
+}
