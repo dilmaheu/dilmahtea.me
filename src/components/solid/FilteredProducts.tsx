06a7a651a7bae90ca070db7d8101c9a8407b245c
RafidMuhymin <rafidmuhymin@gmail.com>
2023-04-07 04:12:00 +0600

    fixed in stock date label not being displayed

--- src/components/solid/FilteredProducts.tsx
+++ src/components/solid/FilteredProducts.tsx
@@ -71,130 +71,124 @@ export default function FilteredProducts({
       class="wrapper grid sm:grid-cols-2 lg:grid-cols-3 gap-[50px] overflow-hidden"
     >
       <For each={filteredProducts()}>
-        {(product, iSignal) => {
-          const inStockDate = new Date(product.In_stock_date).toLocaleString(
-            "en-GB",
-            { year: "2-digit", month: "short", day: "numeric" }
-          );
+        {(product, iSignal) => (
+          <div
+            role="listitem"
+            aria-label={page.Aria_label_tea_item_text + (iSignal() + 1)}
+            style="clip-path: url(#product-card-curve)"
+            class="product-card link-section flex flex-wrap w-full mx-auto max-w-[380px] sm:max-w-none bg-primary"
+          >
+            <div class="relative block w-full">
+              <div innerHTML={product.Intro_blob_HTML} />
 
-          return (
-            <div
-              role="listitem"
-              aria-label={page.Aria_label_tea_item_text + (iSignal() + 1)}
-              style="clip-path: url(#product-card-curve)"
-              class="product-card link-section flex flex-wrap w-full mx-auto max-w-[380px] sm:max-w-none bg-primary"
-            >
-              <div class="relative block w-full">
-                <div innerHTML={product.Intro_blob_HTML} />
-
-                {product.Stock_amount < 1 && (
-                  <>
-                    <div
-                      class={[
-                        "absolute top-[12%] right-[12%] flex items-center px-5 sm:px-[37px] py-[5px] sm:py-[7px]",
-                        "sm:text-xl font-semibold text-dark2 border-2 border-[#fabf21] bg-[#fabf21]/80 rounded-full",
-                      ].join(" ")}
-                    >
-                      {recurData.Product_sold_out_text}
-                    </div>
-
-                    {
-                      // hide label if in-stock date is in the past or undefined
-                      // @ts-ignore
-                      new Date() > inStockDate && (
-                        <div
-                          class={[
-                            "absolute bottom-0 flex items-center gap-2.5 px-[34px] py-2.5 select-none w-full",
-                            "text-sm sm:text-base font-medium text-dark2 bg-[#faf8e5]/80",
-                          ].join(" ")}
-                        >
-                          {alertCircleIcon}
-                          {recurData.Item_stock_text.replace(
-                            "<in_stock_date>",
-                            inStockDate
-                          )}
-                        </div>
-                      )
-                    }
-                  </>
-                )}
-              </div>
-
-              <div class="py-[30px] lg:py-[40px] px-[36px] lg:px-[48px]">
-                <div
-                  class="product-card-title alice leading-[120%] md:leading-[110%]"
-                  style={{ "font-size": "clamp(1.75rem, 2vw + 0.3rem, 2rem)" }}
-                >
-                  <a
-                    aria-label={
-                      (product.Stock_amount < 1
-                        ? `${recurData.Product_sold_out_text}, `
-                        : "") + product.Title
-                    }
-                    class="main-link"
-                    href={product.Meta.URL_slug}
+              {product.Stock_amount < 1 && (
+                <>
+                  <div
+                    class={[
+                      "absolute top-[12%] right-[12%] flex items-center px-5 sm:px-[37px] py-[5px] sm:py-[7px]",
+                      "sm:text-xl font-semibold text-dark2 border-2 border-[#fabf21] bg-[#fabf21]/80 rounded-full",
+                    ].join(" ")}
                   >
-                    {product.Title}
-                  </a>
+                    {recurData.Product_sold_out_text}
+                  </div>
 
-                  <span
-                    class="icon product-card-title-icon"
-                    style={{
-                      height: "clamp(0.625rem, 1.5vw + 0.1rem, 1rem)",
-                    }}
-                  >
-                    <img class="w-full h-full" {...white_right_arrow} />
-                  </span>
-                </div>
-
-                <div
-                  innerHTML={product.Intro_text_HTML}
-                  class="text-base md:text-lg leading-[150%] line-clamp-3 mt-[5px] md:mt-[7px] lg:mt-[15px]"
-                />
-
-                {product.Stock_amount < 1 && (
-                  <div class="mt-[25px]">
-                    <div class="flex flex-wrap gap-x-2.5">
-                      <div class="relative flex">
-                        {product.availableFormatThumbnails.map(
-                          ({ src, alt }) => (
-                            <img
-                              src={src}
-                              alt={alt}
-                              class="w-[26px] h-[26px] border-2 border-primary rounded-full -ml-[16.5px] first:ml-0"
-                            />
+                  {
+                    // hide label if in-stock date is in the past or undefined
+                    new Date(product.In_stock_date).getTime() >
+                      new Date().getTime() && (
+                      <div
+                        class={[
+                          "absolute bottom-0 flex items-center gap-2.5 px-[34px] py-2.5 select-none w-full",
+                          "text-sm sm:text-base font-medium text-dark2 bg-[#faf8e5]/80",
+                        ].join(" ")}
+                      >
+                        {alertCircleIcon}
+                        {recurData.Item_stock_text.replace(
+                          "<in_stock_date>",
+                          new Date(product.In_stock_date).toLocaleString(
+                            "en-GB",
+                            { year: "2-digit", month: "short", day: "numeric" }
                           )
                         )}
-
-                        {product.availableFormatsCount > 2 && (
-                          <div
-                            class={[
-                              "relative -ml-[17px] flex items-center justify-center bg-lightgray2",
-                              "w-[26px] h-[26px] border-2 rounded-full text-sm text-primary leading-[150%]",
-                            ].join(" ")}
-                          >
-                            +{product.availableFormatsCount - 2}
-                          </div>
-                        )}
                       </div>
+                    )
+                  }
+                </>
+              )}
+            </div>
 
-                      <div class="text-lightgray2">
-                        <em>
-                          {product.availableFormatsCount === 1
-                            ? recurData.Product_stock_other_formats_text_singular
-                            : recurData.Product_stock_other_formats_text.replace(
-                                "<count>",
-                                product.availableFormatsCount
-                              )}
-                        </em>
-                      </div>
+            <div class="py-[30px] lg:py-[40px] px-[36px] lg:px-[48px]">
+              <div
+                class="product-card-title alice leading-[120%] md:leading-[110%]"
+                style={{ "font-size": "clamp(1.75rem, 2vw + 0.3rem, 2rem)" }}
+              >
+                <a
+                  aria-label={
+                    (product.Stock_amount < 1
+                      ? `${recurData.Product_sold_out_text}, `
+                      : "") + product.Title
+                  }
+                  class="main-link"
+                  href={product.Meta.URL_slug}
+                >
+                  {product.Title}
+                </a>
+
+                <span
+                  class="icon product-card-title-icon"
+                  style={{
+                    height: "clamp(0.625rem, 1.5vw + 0.1rem, 1rem)",
+                  }}
+                >
+                  <img class="w-full h-full" {...white_right_arrow} />
+                </span>
+              </div>
+
+              <div
+                innerHTML={product.Intro_text_HTML}
+                class="text-base md:text-lg leading-[150%] line-clamp-3 mt-[5px] md:mt-[7px] lg:mt-[15px]"
+              />
+
+              {product.Stock_amount < 1 && (
+                <div class="mt-[25px]">
+                  <div class="flex flex-wrap gap-x-2.5">
+                    <div class="relative flex">
+                      {product.availableFormatThumbnails.map(({ src, alt }) => (
+                        <img
+                          src={src}
+                          alt={alt}
+                          class="w-[26px] h-[26px] border-2 border-primary rounded-full -ml-[16.5px] first:ml-0"
+                        />
+                      ))}
+
+                      {product.availableFormatsCount > 2 && (
+                        <div
+                          class={[
+                            "relative -ml-[17px] flex items-center justify-center bg-lightgray2",
+                            "w-[26px] h-[26px] border-2 rounded-full text-sm text-primary leading-[150%]",
+                          ].join(" ")}
+                        >
+                          +{product.availableFormatsCount - 2}
+                        </div>
+                      )}
+                    </div>
+
+                    <div class="text-lightgray2">
+                      <em>
+                        {product.availableFormatsCount === 1
+                          ? recurData.Product_stock_other_formats_text_singular
+                          : recurData.Product_stock_other_formats_text.replace(
+                              "<count>",
+                              product.availableFormatsCount
+                            )}
+                      </em>
                     </div>
                   </div>
-                )}
-              </div>
+                </div>
+              )}
             </div>
-          );
-        }}
+          </div>
+        )}
       </For>
     </div>
   );
