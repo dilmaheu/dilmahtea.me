46a71382d97e5c9323e1b7f11966fa9dc11bb3c0
RafidMuhymin <rafidmuhymin@gmail.com>
2023-04-26 17:59:41 +0600

    refactored RecurringImages store code

--- src/store/RecurringImages.js
+++ src/store/RecurringImages.js
@@ -6,12 +6,14 @@ const { data: recurringImageData } = CMS.get("recurringImage");
 const RecurringImages = {};
 
 await Promise.all(
-  Object.keys(recurringImageData?.attributes ?? {}).map(async (key) => {
-    const { url: relativeSrc, alternativeText: alt } =
-      recurringImageData.attributes[key]?.data?.attributes ?? {};
+  Object.keys(recurringImageData.attributes).map(async (key) => {
+    const imageData = recurringImageData.attributes[key].data;
 
-    if (relativeSrc) {
+    if (imageData) {
+      const { url: relativeSrc, alternativeText: alt } = imageData.attributes;
+
       const src = await localizeCMSImage(relativeSrc);
+
       RecurringImages[key] = { src, alt };
     }
   })
