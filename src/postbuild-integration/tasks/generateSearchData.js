253e23e637f2b9e91d9ae6593ce5619eabfd1c05
ih-abir <ih.abir.1@gmail.com>
2023-06-02 12:42:00 +0600

    update for generating json with non categorized array & sort alphabetically

--- src/postbuild-integration/tasks/generateSearchData.js
+++ src/postbuild-integration/tasks/generateSearchData.js
@@ -12,73 +12,55 @@ const selectedPages = [
   "blogs",
   "howTos",
   "estates",
-  "products",
-  "productCategories",
   "homepage",
+  "products",
   "homeEstate",
   "homeProduct",
   "homeElephantPath",
+  "productCategories",
 ];
 
-for (const page of selectedPages) {
+const filteredData = selectedPages.reduce((result, page) => {
   if (page in data.data) {
     const pageData = data.data[page];
-    const pageArray = Array.isArray(pageData.data)
-      ? pageData.data
-      : [pageData.data];
-    filteredData[page] = {
-      data: pageArray
-        .map(({ attributes }) => {
-          const filteredObj = {
-            attributes: {
-              locale: attributes.locale,
-              Title: attributes.Title || attributes.Estate_name,
-              Intro_text: attributes.Intro_text.slice(
-                0,
-                60 || attributes.Intro_text.length
-              ).replace(/(<([^>]+)>)/gi, ""),
-              Intro_blob: {
-                url:
-                  CMS_ENDPOINT +
-                  (attributes?.Intro_blob?.data.attributes?.formats?.thumbnail
-                    .url || attributes?.Intro_blob?.data.attributes.url),
-                alt: attributes?.Intro_blob?.data.attributes.alternativeText,
-              },
-              URL_slug: attributes.Meta.URL_slug,
-            },
-          };
+    const pageArray = Array.isArray(pageData.data) ? pageData.data : [pageData.data];
 
-          if (attributes.localizations?.data?.length) {
-            filteredObj.attributes.localizations = {
-              data: attributes.localizations.data.map(({ attributes }) => ({
-                attributes: {
-                  locale: attributes.locale,
-                  Title: attributes.Title || attributes.Estate_name,
-                  Intro_text: attributes.Intro_text.slice(
-                    0,
-                    60 || attributes.Intro_text.length
-                  ).replace(/(<([^>]+)>)/gi, ""),
-                  Intro_blob: {
-                    url:
-                      CMS_ENDPOINT +
-                      (attributes?.Intro_blob?.data.attributes?.formats
-                        ?.thumbnail.url ||
-                        attributes?.Intro_blob?.data.attributes.url),
-                    alt: attributes?.Intro_blob?.data.attributes
-                      .alternativeText,
-                  },
-                  URL_slug: attributes.Meta.URL_slug,
-                },
-              })),
-            };
-          }
+    const pageFilteredData = pageArray.map(({ attributes }) => {
+      const filteredObj = {
+        locale: attributes?.locale,
+        Title: attributes?.Title || attributes?.Estate_name,
+        Intro_text: attributes?.Intro_text?.slice(0, 60).replace(/(<([^>]+)>)/gi, ""),
+        Intro_blob: {
+          url: CMS_ENDPOINT + (attributes?.Intro_blob?.data?.attributes?.formats?.thumbnail?.url || attributes?.Intro_blob?.data?.attributes?.url),
+          alt: attributes?.Intro_blob?.data?.attributes?.alternativeText,
+        },
+        URL_slug: attributes?.Meta?.URL_slug,
+      };
 
-          return filteredObj;
-        })
-        .filter((obj) => Object.keys(obj.attributes).length > 0),
-    };
+      if (attributes?.localizations?.data?.length) {
+        filteredObj.localizations = attributes.localizations.data.map(({ attributes }) => ({
+          locale: attributes?.locale,
+          Title: attributes?.Title || attributes?.Estate_name,
+          Intro_text: attributes?.Intro_text?.slice(0, 60).replace(/(<([^>]+)>)/gi, ""),
+          Intro_blob: {
+            url: CMS_ENDPOINT + (attributes?.Intro_blob?.data?.attributes?.formats?.thumbnail?.url || attributes?.Intro_blob?.data?.attributes?.url),
+            alt: attributes?.Intro_blob?.data?.attributes?.alternativeText,
+          },
+          URL_slug: attributes?.Meta?.URL_slug,
+        }));
+      }
+
+      return filteredObj;
+    }).filter(obj => Object.keys(obj).length > 0);
+
+    result = [...result, ...pageFilteredData];
   }
-}
+
+  return result;
+}, []);
+
+// Sort the filtered data alphabetically by Title
+filteredData.sort((a, b) => a.Title.localeCompare(b.Title));
 
 // write filtered data to SearchData file
 await writeFile(
