df8ad350829815d6ad0cfd593c9f2fa148215f23
ih-abir <ih.abir.1@gmail.com>
2023-06-02 12:50:33 +0600

    format code

--- src/postbuild-integration/tasks/generateSearchData.js
+++ src/postbuild-integration/tasks/generateSearchData.js
@@ -7,7 +7,6 @@ const CMS_ENDPOINT = process.env.ASSETS_URL;
 
 const rawData = await readFile(".cache/CMS.json");
 const data = JSON.parse(rawData);
-const filteredData = {};
 const selectedPages = [
   "blogs",
   "howTos",
@@ -23,35 +22,53 @@ const selectedPages = [
 const filteredData = selectedPages.reduce((result, page) => {
   if (page in data.data) {
     const pageData = data.data[page];
-    const pageArray = Array.isArray(pageData.data) ? pageData.data : [pageData.data];
+    const pageArray = Array.isArray(pageData.data)
+      ? pageData.data
+      : [pageData.data];
 
-    const pageFilteredData = pageArray.map(({ attributes }) => {
-      const filteredObj = {
-        locale: attributes?.locale,
-        Title: attributes?.Title || attributes?.Estate_name,
-        Intro_text: attributes?.Intro_text?.slice(0, 60).replace(/(<([^>]+)>)/gi, ""),
-        Intro_blob: {
-          url: CMS_ENDPOINT + (attributes?.Intro_blob?.data?.attributes?.formats?.thumbnail?.url || attributes?.Intro_blob?.data?.attributes?.url),
-          alt: attributes?.Intro_blob?.data?.attributes?.alternativeText,
-        },
-        URL_slug: attributes?.Meta?.URL_slug,
-      };
-
-      if (attributes?.localizations?.data?.length) {
-        filteredObj.localizations = attributes.localizations.data.map(({ attributes }) => ({
+    const pageFilteredData = pageArray
+      .map(({ attributes }) => {
+        const filteredObj = {
           locale: attributes?.locale,
           Title: attributes?.Title || attributes?.Estate_name,
-          Intro_text: attributes?.Intro_text?.slice(0, 60).replace(/(<([^>]+)>)/gi, ""),
+          Intro_text: attributes?.Intro_text?.slice(0, 60).replace(
+            /(<([^>]+)>)/gi,
+            ""
+          ),
           Intro_blob: {
-            url: CMS_ENDPOINT + (attributes?.Intro_blob?.data?.attributes?.formats?.thumbnail?.url || attributes?.Intro_blob?.data?.attributes?.url),
+            url:
+              CMS_ENDPOINT +
+              (attributes?.Intro_blob?.data?.attributes?.formats?.thumbnail
+                ?.url || attributes?.Intro_blob?.data?.attributes?.url),
             alt: attributes?.Intro_blob?.data?.attributes?.alternativeText,
           },
           URL_slug: attributes?.Meta?.URL_slug,
-        }));
-      }
+        };
 
-      return filteredObj;
-    }).filter(obj => Object.keys(obj).length > 0);
+        if (attributes?.localizations?.data?.length) {
+          filteredObj.localizations = attributes.localizations.data.map(
+            ({ attributes }) => ({
+              locale: attributes?.locale,
+              Title: attributes?.Title || attributes?.Estate_name,
+              Intro_text: attributes?.Intro_text?.slice(0, 60).replace(
+                /(<([^>]+)>)/gi,
+                ""
+              ),
+              Intro_blob: {
+                url:
+                  CMS_ENDPOINT +
+                  (attributes?.Intro_blob?.data?.attributes?.formats?.thumbnail
+                    ?.url || attributes?.Intro_blob?.data?.attributes?.url),
+                alt: attributes?.Intro_blob?.data?.attributes?.alternativeText,
+              },
+              URL_slug: attributes?.Meta?.URL_slug,
+            })
+          );
+        }
+
+        return filteredObj;
+      })
+      .filter((obj) => Object.keys(obj).length > 0);
 
     result = [...result, ...pageFilteredData];
   }
