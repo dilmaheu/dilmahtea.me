d43ab79cf666ac084ade972b516b95e649d9b7c5
RafidMuhymin <rafidmuhymin@gmail.com>
2023-06-09 03:01:12 +0600

    simplified [locale].json.ts logic

--- src/pages/search-index/[locale].json.ts
+++ src/pages/search-index/[locale].json.ts
@@ -1,143 +1,73 @@
 import CMS from "@store/CMS.js";
+import Products from "@store/Products";
 
 const { ASSETS_URL } = import.meta.env,
-  selectedPages = [
-  "blogs",
-  "howTos",
-  "recipes",
-  "estates",
-  "homepage",
-  "products",
-  "homeEstate",
-  "homeProduct",
-  "homeElephantPath",
-  "productCategories",
-];
+  contentTypes = [
+    "blogs",
+    "howTos",
+    "recipes",
+    "estates",
+    "homepage",
+    "products",
+    "homeEstate",
+    "homeProduct",
+    "homeElephantPath",
+    "productCategories",
+  ];
 
-const allCMSData = CMS.get("all");
+const searchIndex = {},
+  pages = contentTypes.flatMap(
+    (type) => (CMS.get(type) || Products.get("all")).data
+  );
 
-function processData(selectedPages, data) {
-  const processedData = {};
+pages.forEach(({ attributes }) => {
+  [
+    attributes,
+    ...attributes.localizations.data.map(({ attributes }) => attributes),
+  ].forEach((attributes) => {
+    const locale = attributes.locale.substring(0, 2),
+      Title = attributes.Title || attributes.Estate_name,
+      Intro_text = attributes.Intro_text.slice(0, 60).replace(
+        /(<([^>]+)>)/gi,
+        ""
+      ),
+      Intro_blob = {
+        url:
+          ASSETS_URL +
+          (attributes.Intro_blob.data.attributes.formats?.thumbnail.url ||
+            attributes.Intro_blob.data.attributes.url),
+        alt: attributes.Intro_blob.data.attributes.alternativeText,
+      },
+      URL_slug = attributes.Meta.URL_slug;
 
-  for (const page of selectedPages) {
-    if (page in data) {
-      const { data: pageData } = data[page];
-      const pageArray = Array.isArray(pageData) ? pageData : [pageData];
+    searchIndex[locale] ||= [];
 
-      for (const { attributes } of pageArray) {
-        // Destructure attributes object
-        const {
-          locale,
-          Intro_text,
-          Intro_blob,
-          Meta,
-          localizations,
-          ...restAttributes
-        } = attributes;
+    searchIndex[locale].push({
+      Title,
+      Intro_text,
+      Intro_blob,
+      URL_slug,
+    });
+  });
+});
 
-        // Process localizations
-        const processedLocalizations = localizations?.data?.map(
-          ({ attributes }) => {
-            const {
-              locale,
-              Intro_text,
-              Intro_blob,
-              URL_slug,
-              ...restLocalizations
-            } = attributes;
-            return {
-              locale,
-              Title: restLocalizations?.Title || restLocalizations?.Estate_name,
-              Intro_text: Intro_text?.slice(0, 60).replace(/(<([^>]+)>)/gi, ""),
-              Intro_blob: {
-                url:
-                  ASSETS_URL +
-                  (Intro_blob?.data?.attributes?.formats?.thumbnail?.url ||
-                    Intro_blob?.data?.attributes?.url),
-                alt: Intro_blob?.data?.attributes?.alternativeText,
-              },
-              URL_slug: Meta.URL_slug,
-            };
-          }
-        );
-
-        // Extract key from locale
-        const key = locale.substring(0, 2);
-
-        // Create compressed item
-        const compressedItem = {
-          Title: restAttributes?.Title || restAttributes?.Estate_name,
-          Intro_text: Intro_text?.slice(0, 60).replace(/(<([^>]+)>)/gi, ""),
-          Intro_blob: {
-            url:
-              ASSETS_URL +
-              (Intro_blob?.data?.attributes?.formats?.thumbnail?.url ||
-                Intro_blob?.data?.attributes?.url),
-            alt: Intro_blob?.data?.attributes?.alternativeText,
-          },
-          URL_slug: Meta.URL_slug,
-        };
-
-        // Store compressed item in processedData
-        if (!(key in processedData)) {
-          processedData[key] = [];
-        }
-        processedData[key].push(compressedItem);
-
-        // Process localizations and store them in processedData
-        if (processedLocalizations?.length) {
-          for (const localization of processedLocalizations) {
-            const { locale, Title, Intro_text, Intro_blob, URL_slug } =
-              localization;
-            const locKey = locale.substring(0, 2);
-            const compressedLocItem = {
-              Title,
-              Intro_text,
-              Intro_blob,
-              URL_slug,
-            };
-
-            if (!(locKey in processedData)) {
-              processedData[locKey] = [];
-            }
-            processedData[locKey].push(compressedLocItem);
-          }
-        }
-      }
-    }
-  }
-
-  // Sort the processedData alphabetically by Title
-  for (const key in processedData) {
-    processedData[key].sort((a, b) => a.Title.localeCompare(b.Title));
-  }
-
-  return processedData;
+// sort the search index alphabetically by Title
+for (const locale in searchIndex) {
+  searchIndex[locale].sort((a, b) => a.Title.localeCompare(b.Title));
 }
 
-const processedData = processData(selectedPages, allCMSData);
-
 export function getStaticPaths() {
-  const staticPaths = Object.keys(processedData).map((key) => {
+  const staticPaths = Object.keys(searchIndex).map((key) => {
     return { params: { locale: key } };
   });
 
-  const jsonData = staticPaths.map((path) => {
-    const { params } = path;
-    const locales = get({ params });
-    return {
-      params,
-      data: locales.body,
-    };
-  });
-
-  return jsonData;
+  return staticPaths;
 }
 
 export function get({ params: { locale } }) {
-  const locales = processedData[locale];
+  const localizedIndex = searchIndex[locale];
 
   return {
-    body: JSON.stringify(locales),
+    body: JSON.stringify(localizedIndex),
   };
 }
