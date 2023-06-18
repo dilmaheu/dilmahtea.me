e3c9d8b5aa68316936c54ef700b5be8327cd966e
RafidMuhymin <rafidmuhymin@gmail.com>
2023-06-18 22:48:56 +0600

    added default route & translation not found redirect support

--- src/utils/getAvailableLocales.ts
+++ src/utils/getAvailableLocales.ts
@@ -1,17 +1,24 @@
 export default function getAvailableLocales(
   flattenedPages: any[]
-): [string[], [string, string][]] {
+): [string[], { [locale: string]: string }, string[]] {
   const availableLocales: string[] = [],
-    availableLocaleEntries: [string, string][] = [];
+    availableLocaleEntries: [string, string][] = [],
+    availableSlugs: string[] = [];
 
   flattenedPages.forEach((attributes) => {
     const locale = (attributes.locale as string).substring(0, 2),
       slug = attributes.Meta?.URL_slug as string,
-      fullSlug = "/" + (slug === "/" ? locale : locale + "/" + slug) + "/";
+      baseSlug = "/" + (slug === "/" ? "" : slug + "/"),
+      fullSlug = "/" + locale + baseSlug;
 
     availableLocales.push(locale);
     availableLocaleEntries.push([locale, fullSlug]);
+    availableSlugs.push(baseSlug);
   });
 
-  return [availableLocales, availableLocaleEntries];
+  return [
+    availableLocales,
+    Object.fromEntries(availableLocaleEntries),
+    [...new Set(availableSlugs)],
+  ];
 }
