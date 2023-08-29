40c73e93ed3811e28576b6b2c9c626ac869cf031
ih-abir <ih.abir.1@gmail.com>
2023-08-29 18:35:44 +0600

    sort products by rank

--- src/pages/db/products/[...key].json.ts
+++ src/pages/db/products/[...key].json.ts
@@ -14,6 +14,7 @@ async function processProductData(attributes) {
   const {
     locale,
     SKU,
+    rank,
     Title,
     names,
     Intro_text,
@@ -108,6 +109,7 @@ async function processProductData(attributes) {
 
   return {
     SKU,
+    rank,
     Title,
     names: JSON.stringify(names),
     Intro_blob_HTML,
