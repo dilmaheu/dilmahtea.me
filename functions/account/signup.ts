c17bd1791e684a225c82b1f7bbf7c67e8b714032
RafidMuhymin <rafidmuhymin@gmail.com>
2024-01-07 06:20:26 +0600

    update or create contact on signup if account exists in exact

--- functions/account/signup.ts
+++ functions/account/signup.ts
@@ -58,72 +58,133 @@ export const onRequestPost: PagesFunction<ENV> = async (context) => {
     },
   });
 
-  const Name = first_name + " " + last_name,
-    Language = locale;
+  const FirstName = first_name,
+    LastName = last_name,
+    Name = FirstName + " " + LastName,
+    Language = locale,
+    CustomerFilter = `${ProviderId} eq '${contact.toLowerCase()}'`;
 
-  const ExistingCustomer = await fetchExactAPI(
-    "GET",
-    "/crm/Accounts?$select=ID,Name,Language,Email,Phone,Country&$filter=" +
-      `${ProviderId} eq '${contact.toLowerCase()}'`,
-    env,
-  ).then(({ feed }) => feed.entry?.content["m:properties"]);
+  try {
+    const ExistingCustomer = await fetchExactAPI(
+      "GET",
+      "/crm/Accounts?$select=ID,Name,Language,Email,Phone,Country&$filter=" +
+        `${ProviderId} eq '${contact.toLowerCase()}'`,
+      env,
+    ).then(({ feed }) => feed.entry?.content["m:properties"]);
 
-  if (ExistingCustomer) {
-    const alternateProviderId = providerId === "email" ? "phone" : "email",
-      alternateProviderUserId =
-        providerId === "email"
-          ? ExistingCustomer["d:Phone"] &&
-            formatNumber(
-              ExistingCustomer["d:Phone"],
-              ExistingCustomer["d:Country"],
-            )
-          : ExistingCustomer["d:Email"];
+    if (ExistingCustomer) {
+      const alternateProviderId = providerId === "email" ? "phone" : "email",
+        alternateProviderUserId =
+          providerId === "email"
+            ? ExistingCustomer["d:Phone"] &&
+              formatNumber(
+                ExistingCustomer["d:Phone"],
+                ExistingCustomer["d:Country"],
+              )
+            : ExistingCustomer["d:Email"];
 
-    if (alternateProviderUserId) {
-      const { userId } = user;
+      if (alternateProviderUserId) {
+        const { userId } = user;
 
-      await auth.createKey({
-        userId,
-        providerId: alternateProviderId,
-        providerUserId: alternateProviderUserId.toLowerCase(),
-        password: null,
-      });
+        await auth.createKey({
+          userId,
+          providerId: alternateProviderId,
+          providerUserId: alternateProviderUserId.toLowerCase(),
+          password: null,
+        });
 
-      await auth.updateUserAttributes(userId, {
-        [alternateProviderId]: alternateProviderUserId,
-      });
-    }
+        await auth.updateUserAttributes(userId, {
+          [alternateProviderId]: alternateProviderUserId,
+        });
+      }
 
-    if (
-      ExistingCustomer["d:Name"] !== Name ||
-      ExistingCustomer["d:Language"] !== Language
-    ) {
-      await fetchExactAPI(
-        "PUT",
-        `/crm/Accounts(guid'${ExistingCustomer["d:ID"]}')`,
+      const promises = [];
+
+      const Contact = await fetchExactAPI(
+        "GET",
+        `/CRM/Contacts?$filter=Account eq guid'${ExistingCustomer["d:ID"]}' and ${CustomerFilter}&select=ID,FirstName,LastName`,
         env,
-        {
-          Name,
-          Language,
-        },
-      );
+      ).then(async ({ feed }) => {
+        const matchedContact = [feed.entry]
+          .flat()
+          .filter(Boolean)
+          .find(({ content: { "m:properties": props } }) => {
+            return [Name, ExistingCustomer["d:Name"]].includes(
+              props["d:FirstName"] + " " + props["d:LastName"],
+            );
+          })?.content["m:properties"];
+
+        if (matchedContact) {
+          if (
+            matchedContact["d:FirstName"] !== FirstName ||
+            matchedContact["d:LastName"] !== LastName
+          ) {
+            promises.push(
+              fetchExactAPI(
+                "PUT",
+                `/CRM/Contacts(guid'${matchedContact["d:ID"]}')`,
+                env,
+                { FirstName, LastName },
+              ),
+            );
+          }
+
+          return matchedContact;
+        } else {
+          // create a new contact if there is no contact or exact match
+          const newContact = await fetchExactAPI("POST", "/CRM/Contacts", env, {
+            Account: ExistingCustomer["d:ID"],
+            FirstName,
+            LastName,
+            [ProviderId]: contact.toLowerCase(),
+          });
+
+          return newContact.entry.content["m:properties"];
+        }
+      });
+
+      if (
+        ExistingCustomer["d:Name"] !== Name ||
+        ExistingCustomer["d:Language"] !== Language ||
+        ExistingCustomer["d:MainContact"] !== Contact["d:ID"]
+      ) {
+        promises.push(
+          fetchExactAPI(
+            "PUT",
+            `/crm/Accounts(guid'${ExistingCustomer["d:ID"]}')`,
+            env,
+            {
+              Name,
+              Language,
+              MainContact: Contact["d:ID"],
+            },
+          ),
+        );
+      }
+
+      await Promise.all(promises);
+    } else {
+      const Customer = await fetchExactAPI("POST", "/crm/Accounts", env, {
+        [ProviderId]: contact.toLowerCase(),
+        Name,
+        Language,
+        Status: "C",
+      });
+
+      const customerID = Customer.entry.content["m:properties"]["d:ID"];
+
+      await fetchExactAPI("POST", "/CRM/Contacts", env, {
+        Account: customerID,
+        FirstName,
+        LastName,
+        [ProviderId]: contact.toLowerCase(),
+      });
     }
-  } else {
-    const Customer = await fetchExactAPI("POST", "/crm/Accounts", env, {
-      [ProviderId]: contact.toLowerCase(),
-      Name,
-      Language,
-      Status: "C",
-    });
-
-    const customerID = Customer.entry.content["m:properties"]["d:ID"];
-
-    await fetchExactAPI("POST", "/CRM/Contacts", env, {
-      Account: customerID,
-      FirstName: first_name,
-      LastName: last_name,
-      [ProviderId]: contact.toLowerCase(),
-    });
+  } catch (error) {
+    return Response.json(
+      { success: false, message: error.message },
+      { status: 400 },
+    );
   }
 
   await removeToken(env.USERS, token);
