d2837202b67503077f703cd36bc4c61b5c5bd382
RafidMuhymin <rafidmuhymin@gmail.com>
2024-01-15 05:43:11 +0600

    updated formatNumber logic

--- functions/utils/formatNumber.ts
+++ functions/utils/formatNumber.ts
@@ -3,25 +3,10 @@ import phoneUtil from "google-libphonenumber";
 const phoneUtilInstance = phoneUtil.PhoneNumberUtil.getInstance(),
   { E164 } = phoneUtil.PhoneNumberFormat;
 
-export default function formatNumber(
-  number: string | number,
-  country: string,
-): string {
-  number = String(number);
+export default function formatNumber(number: string, country: string): string {
+  number = phoneUtilInstance.parse(number, country);
 
-  try {
-    const parsedNumber = phoneUtilInstance.parse(
-      number.startsWith("+") ? number : "+" + number,
-    );
+  number = phoneUtilInstance.format(number, E164);
 
-    if (!phoneUtilInstance.isValidNumber(parsedNumber)) throw new Error();
-
-    number = phoneUtilInstance.format(parsedNumber, E164);
-  } catch (error) {
-    number = phoneUtilInstance.parse(number, country);
-
-    number = phoneUtilInstance.format(number, E164);
-  }
-
-  return number as string;
+  return number;
 }
