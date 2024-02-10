48acf627e691d1b6c605cd309be4736112ac68ea
ih-abir <ih.abir.1@gmail.com>
2024-02-10 21:43:54 +0600

    update product button and price font

--- src/components/solid/FilteredProducts.tsx
+++ src/components/solid/FilteredProducts.tsx
@@ -67,8 +67,8 @@ export default function FilteredProducts({
           productA.rank === null
             ? 1
             : productB.rank === null
-            ? -1
-            : productA.rank - productB.rank,
+              ? -1
+              : productA.rank - productB.rank,
         ),
     );
   });
@@ -146,8 +146,7 @@ export default function FilteredProducts({
                   </a>
                 </div>
 
-                <div
-                  class="product-info">
+                <div class="product-info">
                   {(product.tea_size || product.Weight_tea) &&
                     (product.tea_size?.toLowerCase().includes("bag")
                       ? product.productLocalizedSize
@@ -162,134 +161,122 @@ export default function FilteredProducts({
                 </div>
 
                 {product.availableFormatsCount > 0 && (
-                <div class="product-format">
-                  <span class="icon-container">
-                    {product.availableFormatThumbnails.map(({ src, alt }) => (
-                      <img
-                        src={src}
-                        alt={alt}
-                        class="icon"
-                      />
-                    ))}
+                  <div class="product-format">
+                    <span class="icon-container">
+                      {product.availableFormatThumbnails.map(({ src, alt }) => (
+                        <img src={src} alt={alt} class="icon" />
+                      ))}
 
-                    {product.availableFormatsCount > 2 && (
-                      <span class="number">+{product.availableFormatsCount - 2}</span>
+                      {product.availableFormatsCount > 2 && (
+                        <span class="number">
+                          +{product.availableFormatsCount - 2}
+                        </span>
+                      )}
+                    </span>
+
+                    <span class="desktop-only-text">
+                      {product.Stock_amount === 0
+                        ? recurData.Product_stock_available_text
+                        : recurData.Product_available_text}
+                    </span>
+
+                    <span
+                      class={[
+                        "desktop-only-text format-link-text",
+                        product.Stock_amount === 0 && "text-white",
+                      ].join(" ")}
+                    >
+                      {product.availableFormatsCount === 1
+                        ? recurData.Product_other_formats_singular_text
+                        : recurData.Product_other_formats_text.replaceAll(
+                            "<count>",
+                            product.availableFormatsCount,
+                          )}
+                    </span>
+
+                    <span
+                      class={[
+                        "mobile-only-text format-link-text",
+                        product.Stock_amount === 0 && "text-white",
+                      ].join(" ")}
+                    >
+                      {product.availableFormatsCount === 1
+                        ? recurData.Product_other_formats_singular_text_sm
+                        : recurData.Product_other_formats_text_sm.replaceAll(
+                            "<count>",
+                            product.availableFormatsCount,
+                          )}
+                    </span>
+                  </div>
+                )}
+
+                {product.estate_name.data.length > 0 && (
+                  <div class="product-estate">
+                    <img class="icon" {...white_love} />
+
+                    <div class="hidden sm:inline">
+                      {recurData.product_made_love_from}
+                    </div>
+
+                    <div class="inline sm:hidden">{recurData.product_from}</div>
+
+                    {product.estate_name?.data.map(
+                      (
+                        {
+                          attributes: {
+                            Estate_name,
+                            Meta: { URL_slug },
+                          },
+                        },
+                        index,
+                      ) => (
+                        <a href={URL_slug}>
+                          {Estate_name +
+                            (index === product.estate_name.data.length - 1
+                              ? ""
+                              : ",")}
+                        </a>
+                      ),
                     )}
-                  </span>
-
-                  
-                  <span class="desktop-only-text">
-                    {product.Stock_amount === 0
-                      ? recurData.Product_stock_available_text
-                      : recurData.Product_available_text}
-                  </span>
-
-                  <span class={[
-                    "desktop-only-text format-link-text",
-                    product.Stock_amount === 0 && "text-white",
-                  ].join(" ")}
-                  >
-                    {product.availableFormatsCount === 1
-                      ? recurData.Product_other_formats_singular_text
-                      : recurData.Product_other_formats_text.replaceAll(
-                          "<count>",
-                          product.availableFormatsCount,
-                        )}
-                  </span>
-                      
-                  <span class={[
-                    "mobile-only-text format-link-text",
-                    product.Stock_amount === 0 && "text-white",
-                  ].join(" ")}
-                  >
-                    {product.availableFormatsCount === 1
-                      ? recurData.Product_other_formats_singular_text_sm
-                      : recurData.Product_other_formats_text_sm.replaceAll(
-                          "<count>",
-                          product.availableFormatsCount,
-                        )}
-                  </span>
-                </div>
-              )}
-
-
-              {product.estate_name.data.length > 0 && (
-                <div class="product-estate">
-                  <img class="icon" {...white_love} />
-
-                  <div class="hidden sm:inline">
-                    {recurData.product_made_love_from}
                   </div>
-
-                  <div class="inline sm:hidden">
-                    {recurData.product_from}
-                  </div>
-
-                  {product.estate_name?.data.map(
-                    (
-                      {attributes: {
-                        Estate_name,
-                        Meta: { URL_slug },
-                      },
-                    },
-                    index,
-                    ) => (
-                      <a href={URL_slug}>
-                        {Estate_name +
-                          (index === product.estate_name.data.length - 1 ? "" : ",")}
-                      </a>
-                    ),
-                  )}
-                </div>
-              )}
+                )}
               </div>
 
-              {product.Stock_amount > 0 ? (
-                <button
-                  onClick={() => addProductToCart(product)}
-                  class="unlink product-card-btn card-button-cart-default"
+              <button
+                onClick={() => {
+                  if (product.Stock_amount > 0) {
+                    addProductToCart(product);
+                  }
+                }}
+                class={[
+                  "unlink product-card-btn",
+                  product.Stock_amount > 0
+                    ? "card-button-cart-default"
+                    : "card-button-cart-disabled",
+                ].join(" ")}
+                disabled={product.Stock_amount === 0}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  class={[
+                    "cart-icon",
+                    product.Stock_amount > 0 ? "fill-primary" : "fill-white",
+                  ].join(" ")}
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    class="cart-icon"
-                  >
-                    <path
-                      d="M6.01 16.136L4.141 4H3a1 1 0 0 1 0-2h1.985a.993.993 0 0 1 .66.235a.997.997 0 0 1 .346.627L6.319 5H14v2H6.627l1.23 8h9.399l1.5-5h2.088l-1.886 6.287A1 1 0 0 1 18 17H7.016a.993.993 0 0 1-.675-.248a.998.998 0 0 1-.332-.616zM10 20a2 2 0 1 1-4 0a2 2 0 0 1 4 0zm9 0a2 2 0 1 1-4 0a2 2 0 0 1 4 0zm0-18a1 1 0 0 1 1 1v1h1a1 1 0 1 1 0 2h-1v1a1 1 0 1 1-2 0V6h-1a1 1 0 1 1 0-2h1V3a1 1 0 0 1 1-1z"
-                      style="fill:#547b7d"
-                    />
-                  </svg>
-                  <div class="text-container">
-                    {checkoutRecurData.text_add}
-                    <span>&#x025CF;</span>
-                    <span class="recoleta">
-                      {`€` +
-                        product.PriceIncludingTax.toFixed(2).replace(".", ",")}
-                    </span>
-                  </div>
-                </button>
-              ) : (
-                <div class="unlink product-card-btn card-button-cart-disabled">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    class="cart-icon"
-                  >
-                    <path
-                      d="M6.01 16.136L4.141 4H3a1 1 0 0 1 0-2h1.985a.993.993 0 0 1 .66.235a.997.997 0 0 1 .346.627L6.319 5H14v2H6.627l1.23 8h9.399l1.5-5h2.088l-1.886 6.287A1 1 0 0 1 18 17H7.016a.993.993 0 0 1-.675-.248a.998.998 0 0 1-.332-.616zM10 20a2 2 0 1 1-4 0a2 2 0 0 1 4 0zm9 0a2 2 0 1 1-4 0a2 2 0 0 1 4 0zm0-18a1 1 0 0 1 1 1v1h1a1 1 0 1 1 0 2h-1v1a1 1 0 1 1-2 0V6h-1a1 1 0 1 1 0-2h1V3a1 1 0 0 1 1-1z"
-                      style="fill:#fff"
-                    />
-                  </svg>
-                  <div class="text-container">
-                    {recurData.Product_sold_out_text}
-                    <span>&#x025CF;</span>
-                    <span class="recoleta">
-                      {`€` +
-                        product.PriceIncludingTax.toFixed(2).replace(".", ",")}
-                    </span>
-                  </div>
+                  <path d="M6.01 16.136L4.141 4H3a1 1 0 0 1 0-2h1.985a.993.993 0 0 1 .66.235a.997.997 0 0 1 .346.627L6.319 5H14v2H6.627l1.23 8h9.399l1.5-5h2.088l-1.886 6.287A1 1 0 0 1 18 17H7.016a.993.993 0 0 1-.675-.248a.998.998 0 0 1-.332-.616zM10 20a2 2 0 1 1-4 0a2 2 0 0 1 4 0zm9 0a2 2 0 1 1-4 0a2 2 0 0 1 4 0zm0-18a1 1 0 0 1 1 1v1h1a1 1 0 1 1 0 2h-1v1a1 1 0 1 1-2 0V6h-1a1 1 0 1 1 0-2h1V3a1 1 0 0 1 1-1z" />
+                </svg>
+                <div class="text-container">
+                  {product.Stock_amount > 0
+                    ? checkoutRecurData.text_add
+                    : recurData.Product_sold_out_text}
+                  <span>&#x025CF;</span>
+                  <span>
+                    {"€" +
+                      product.PriceIncludingTax.toFixed(2).replace(".", ",")}
+                  </span>
                 </div>
-              )}
+              </button>
             </div>
           </div>
         )}
