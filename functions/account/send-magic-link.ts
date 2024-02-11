3310f7e91e7f648e0ec20cd9326e6bb9169145a0
RafidMuhymin <rafidmuhymin@gmail.com>
2024-02-12 02:34:36 +0600

    added addresses post api

--- functions/account/send-magic-link.ts
+++ functions/account/send-magic-link.ts
@@ -3,10 +3,10 @@ import type { ENV } from "../utils/types";
 
 import { z } from "zod";
 import validator from "validator";
-import { fromZodError } from "zod-validation-error";
 
 import fetchExactAPI from "../utils/fetchExactAPI";
 import getCustomerFilter from "../utils/getCustomerFilter";
+import stringifyZodError from "../utils/stringifyZodError";
 
 import { initializeLucia } from "../utils/auth";
 import { getToken, removeToken, validateToken } from "../utils/token";
@@ -76,11 +76,7 @@ export const onRequestPost: PagesFunction<ENV> = async (context) => {
     try {
       var bodyData = BodySchema.parse(body);
     } catch (error) {
-      throw new PublicError(
-        fromZodError(error)
-          .toString()
-          .replace(/^Validation error: | at "[\w]+"$/g, ""),
-      );
+      throw new PublicError(stringifyZodError(error));
     }
 
     switch (bodyData.action) {
--- functions/api/addresses.ts
+++ functions/api/addresses.ts
@@ -1,54 +1,175 @@
 import type { Session } from "lucia";
 import type { Address } from "@solid/Address";
 
+import { z } from "zod";
+
 import { initializeLucia } from "../utils/auth";
 
 declare interface Env {
   USERS: D1Database;
 }
+const EditAddressBodySchema = z.object({
+  first_name: z.string(),
+  last_name: z.string(),
+  street: z.string(),
+  city: z.string(),
+  postal_code: z.string(),
+  country: z.string(),
+  set_as_default_delivery_address: z.boolean().optional(),
+  set_as_default_billing_address: z.boolean().optional(),
+});
 
-export const onRequestGet: PagesFunction<Env> = async (context) => {
-  const { request, env } = context;
+const AddAddressBodySchema = EditAddressBodySchema.extend({
+  tag: z.string().optional(),
+});
 
-  const auth = initializeLucia(env.USERS),
-    authRequest = auth.handleRequest(request);
+const UpdateAddressBodySchema = EditAddressBodySchema.extend({
+  tag: z.string(),
+});
 
-  const session: Session = await authRequest.validate();
+const DeleteAddressBodySchema = z.object({
+  id: z.string(),
+});
 
-  const {
-    exact_account_guid,
-    default_delivery_address,
-    default_billing_address,
-  } = session.user;
+type AddAddressBody = z.infer<typeof AddAddressBodySchema>;
+type UpdateAddressBody = z.infer<typeof UpdateAddressBodySchema>;
+type DeleteAddressBody = z.infer<typeof DeleteAddressBodySchema>;
 
-  const { results: addresses } = await env.USERS.prepare(
-    "SELECT * FROM addresses WHERE exact_account_guid = ?",
-  )
-    .bind(exact_account_guid)
-    .all<Address>();
+declare type HandlerBody = (
+  env: Env,
+  session: Session,
+  bodyData: any,
+) => Promise<Response>;
 
-  return Response.json(
-    [
-      addresses.find((address, i) => {
-        const isDefaultDeliveryAddress =
-          default_delivery_address === address.id;
+function getAPIHandler(handlerBody: HandlerBody) {
+  const handler: PagesFunction<Env> = async (context) => {
+    const { request, env } = context;
 
-        if (isDefaultDeliveryAddress) {
-          delete addresses[i];
+    const auth = initializeLucia(env.USERS),
+      authRequest = auth.handleRequest(request);
 
-          return isDefaultDeliveryAddress;
+    const session: Session = await authRequest.validate();
+
+    if (!session)
+      return Response.json(
+        { success: false, error: "Unauthorized" },
+        { status: 401 },
+      );
+
+    const bodyData = request.method === "GET" ? null : await request.json();
+
+    try {
+      const Schema =
+        request.method === "POST"
+          ? AddAddressBodySchema
+          : request.method === "PUT"
+            ? UpdateAddressBodySchema
+            : request.method === "DELETE"
+              ? DeleteAddressBodySchema
+              : null;
+
+      var validatedData = Schema?.parse(bodyData) || null;
+    } catch (error) {
+      return Response.json(
+        { success: false, error: "Invalid address details" },
+        { status: 400 },
+      );
+    }
+
+    return await handlerBody(env, session, validatedData);
+  };
+
+  return handler;
+}
+
+export const onRequestGet: PagesFunction<Env> = getAPIHandler(
+  async (env, session) => {
+    const {
+      exact_account_guid,
+      default_delivery_address,
+      default_billing_address,
+    } = session.user;
+
+    const { results: addresses } = await env.USERS.prepare(
+      "SELECT * FROM addresses WHERE exact_account_guid = ?",
+    )
+      .bind(exact_account_guid)
+      .all<Address>();
+
+    return Response.json(
+      [
+        addresses.find((address, i) => {
+          const isDefaultDeliveryAddress =
+            default_delivery_address === address.id;
+
+          if (isDefaultDeliveryAddress) {
+            delete addresses[i];
+
+            return isDefaultDeliveryAddress;
+          }
+        }),
+        addresses.find((address, i) => {
+          const isDefaultBillingAddress =
+            default_billing_address === address.id;
+
+          if (isDefaultBillingAddress) {
+            delete addresses[i];
+
+            return isDefaultBillingAddress;
+          }
+        }),
+        ...addresses.map((address) => address.id),
+      ].filter(Boolean),
+    );
+  },
+);
+
+export const onRequestPost: PagesFunction<Env> = getAPIHandler(
+  async (env, session, validatedData: AddAddressBody) => {
+    const { exact_account_guid } = session.user;
+
+    const { results: addresses } = await env.USERS.prepare(
+      "SELECT * FROM addresses WHERE exact_account_guid = ?",
+    )
+      .bind(exact_account_guid)
+      .all<Address>();
+
+    const usedTags = addresses.map((address) => address.tag);
+
+    if (usedTags.includes(validatedData.tag))
+      return Response.json(
+        { success: false, error: "Address tag already exists" },
+        { status: 400 },
+      );
+
+    if (!validatedData.tag) {
+      let latestCustomAddressTag = 0;
+
+      for (const tag of usedTags) {
+        if (tag.startsWith("Address #")) {
+          const tagNumber = Number(tag.slice(9));
+
+          if (tagNumber > latestCustomAddressTag)
+            latestCustomAddressTag = tagNumber;
         }
-      }),
-      addresses.find((address, i) => {
-        const isDefaultBillingAddress = default_billing_address === address.id;
+      }
 
-        if (isDefaultBillingAddress) {
-          delete addresses[i];
+      validatedData.tag = `Address #${latestCustomAddressTag + 1}`;
+    }
 
-          return isDefaultBillingAddress;
-        }
-      }),
-      ...addresses.map((address) => address.id),
-    ].filter(Boolean),
-  );
-};
+    if (usedTags.length === 0) {
+      validatedData.set_as_default_delivery_address = true;
+      validatedData.set_as_default_billing_address = true;
+    }
+
+    const id = crypto.randomUUID();
+
+    await env.USERS.prepare(
+      "INSERT INTO addresses (id, exact_account_guid, tag, first_name, last_name, street, city, postal_code, country, set_as_default_delivery_address, set_as_default_billing_address) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+    )
+      .bind(id, exact_account_guid, ...Object.values(validatedData))
+      .run();
+
+    return Response.json({ id });
+  },
+);
