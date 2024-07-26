2b0023f210d1a9b9b9d67c609258437c64ec0927
Rafid Muhymin Wafi <rafidmuhymin@gmail.com>
2024-07-26 14:54:11 +0000

    configured all add to cart btns to redirect to shopify

--- src/components/solid/FilteredProducts.tsx
+++ src/components/solid/FilteredProducts.tsx
@@ -229,21 +229,22 @@ export default function FilteredProducts({
                 )}
               </div>
 
-              <button
-                // onClick={() => {
-                //   if (product.Stock_amount > 0) {
-                //     window.addProductToCart(product.SKU);
-                //   }
-                // }}
-                class={[
-                  "unlink product-card-btn",
-                  product.Stock_amount > 0
-                    ? "card-button-cart-default"
-                    : "card-button-cart-disabled",
-                ].join(" ")}
-                disabled={product.Stock_amount === 0}
-              >
-                <svg
+              <a href={product.Shopify_URL}>
+                <button
+                  // onClick={() => {
+                  //   if (product.Stock_amount > 0) {
+                  //     window.addProductToCart(product.SKU);
+                  //   }
+                  // }}
+                  class={[
+                    "unlink product-card-btn",
+                    product.Stock_amount > 0
+                      ? "card-button-cart-default"
+                      : "card-button-cart-disabled",
+                  ].join(" ")}
+                  disabled={product.Stock_amount === 0}
+                >
+                  {/* <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
                   class={[
@@ -252,18 +253,21 @@ export default function FilteredProducts({
                   ].join(" ")}
                 >
                   <path d="M6.01 16.136L4.141 4H3a1 1 0 0 1 0-2h1.985a.993.993 0 0 1 .66.235a.997.997 0 0 1 .346.627L6.319 5H14v2H6.627l1.23 8h9.399l1.5-5h2.088l-1.886 6.287A1 1 0 0 1 18 17H7.016a.993.993 0 0 1-.675-.248a.998.998 0 0 1-.332-.616zM10 20a2 2 0 1 1-4 0a2 2 0 0 1 4 0zm9 0a2 2 0 1 1-4 0a2 2 0 0 1 4 0zm0-18a1 1 0 0 1 1 1v1h1a1 1 0 1 1 0 2h-1v1a1 1 0 1 1-2 0V6h-1a1 1 0 1 1 0-2h1V3a1 1 0 0 1 1-1z" />
-                </svg>
-                <div class="text-container">
-                  {product.Stock_amount > 0
+                </svg> */}
+
+                  <div class="text-container">
+                    {/* {product.Stock_amount > 0
                     ? recurData.text_add
-                    : recurData.text_sold_out}
-                  <span>&#x025CF;</span>
-                  <span>
-                    {"€" +
-                      product.PriceIncludingTax.toFixed(2).replace(".", ",")}
-                  </span>
-                </div>
-              </button>
+                    : recurData.text_sold_out} */}
+                    {recurData.text_buy}
+                    <span>&#x025CF;</span>
+                    <span>
+                      {"€" +
+                        product.PriceIncludingTax.toFixed(2).replace(".", ",")}
+                    </span>
+                  </div>
+                </button>
+              </a>
             </div>
           </div>
         )}
