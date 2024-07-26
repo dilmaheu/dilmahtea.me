3ccc70b1638c550762bafa91166c67f772fc9194
Rafid Muhymin Wafi <rafidmuhymin@gmail.com>
2024-07-26 15:33:55 +0000

    removed all stock out related functionalities

--- src/components/solid/FilteredProducts.tsx
+++ src/components/solid/FilteredProducts.tsx
@@ -89,7 +89,7 @@ export default function FilteredProducts({
             <div class="product-card-image-container">
               <div innerHTML={product.Intro_blob_HTML} />
 
-              {product.Stock_amount < 1 && (
+              {/* {product.Stock_amount < 1 && (
                 <>
                   <div class="stock-out-label-top">
                     {recurData.text_sold_out}
@@ -114,7 +114,7 @@ export default function FilteredProducts({
                     )
                   }
                 </>
-              )}
+              )} */}
             </div>
 
             <div class="card-content-container">
@@ -122,9 +122,9 @@ export default function FilteredProducts({
                 <div class="product-title">
                   <a
                     aria-label={
-                      (product.Stock_amount < 1
+                      /* (product.Stock_amount < 1
                         ? `${recurData.text_sold_out}, `
-                        : "") + product.Title
+                        : "") + */ product.Title
                     }
                     class="main-link"
                     href={product.Meta.URL_slug}
@@ -162,15 +162,17 @@ export default function FilteredProducts({
                     </span>
 
                     <span class="desktop-only-text">
-                      {product.Stock_amount === 0
-                        ? recurData.Product_stock_available_text
-                        : recurData.Product_available_text}
+                      {
+                        /* product.Stock_amount === 0 */ false
+                          ? recurData.Product_stock_available_text
+                          : recurData.Product_available_text
+                      }
                     </span>
 
                     <span
                       class={[
                         "desktop-only-text format-link-text",
-                        product.Stock_amount === 0 && "text-white",
+                        // product.Stock_amount === 0 && "text-white",
                       ].join(" ")}
                     >
                       {product.availableFormatsCount === 1
@@ -184,7 +186,7 @@ export default function FilteredProducts({
                     <span
                       class={[
                         "mobile-only-text format-link-text",
-                        product.Stock_amount === 0 && "text-white",
+                        // product.Stock_amount === 0 && "text-white",
                       ].join(" ")}
                     >
                       {product.availableFormatsCount === 1
@@ -238,27 +240,27 @@ export default function FilteredProducts({
                   // }}
                   class={[
                     "unlink product-card-btn",
-                    product.Stock_amount > 0
+                    /* product.Stock_amount > 0 */ true
                       ? "card-button-cart-default"
                       : "card-button-cart-disabled",
                   ].join(" ")}
-                  disabled={product.Stock_amount === 0}
+                  // disabled={product.Stock_amount === 0}
                 >
                   {/* <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  class={[
-                    "cart-icon",
-                    product.Stock_amount > 0 ? "fill-primary" : "fill-white",
-                  ].join(" ")}
-                >
-                  <path d="M6.01 16.136L4.141 4H3a1 1 0 0 1 0-2h1.985a.993.993 0 0 1 .66.235a.997.997 0 0 1 .346.627L6.319 5H14v2H6.627l1.23 8h9.399l1.5-5h2.088l-1.886 6.287A1 1 0 0 1 18 17H7.016a.993.993 0 0 1-.675-.248a.998.998 0 0 1-.332-.616zM10 20a2 2 0 1 1-4 0a2 2 0 0 1 4 0zm9 0a2 2 0 1 1-4 0a2 2 0 0 1 4 0zm0-18a1 1 0 0 1 1 1v1h1a1 1 0 1 1 0 2h-1v1a1 1 0 1 1-2 0V6h-1a1 1 0 1 1 0-2h1V3a1 1 0 0 1 1-1z" />
-                </svg> */}
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    class={[
+                      "cart-icon",
+                      product.Stock_amount > 0 ? "fill-primary" : "fill-white",
+                    ].join(" ")}
+                  >
+                    <path d="M6.01 16.136L4.141 4H3a1 1 0 0 1 0-2h1.985a.993.993 0 0 1 .66.235a.997.997 0 0 1 .346.627L6.319 5H14v2H6.627l1.23 8h9.399l1.5-5h2.088l-1.886 6.287A1 1 0 0 1 18 17H7.016a.993.993 0 0 1-.675-.248a.998.998 0 0 1-.332-.616zM10 20a2 2 0 1 1-4 0a2 2 0 0 1 4 0zm9 0a2 2 0 1 1-4 0a2 2 0 0 1 4 0zm0-18a1 1 0 0 1 1 1v1h1a1 1 0 1 1 0 2h-1v1a1 1 0 1 1-2 0V6h-1a1 1 0 1 1 0-2h1V3a1 1 0 0 1 1-1z" />
+                  </svg> */}
 
                   <div class="text-container">
                     {/* {product.Stock_amount > 0
-                    ? recurData.text_add
-                    : recurData.text_sold_out} */}
+                      ? recurData.text_add
+                      : recurData.text_sold_out} */}
                     {recurData.text_buy}
                     <span>&#x025CF;</span>
                     <span>
